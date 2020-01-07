1bb353b560567fb3ac1daeb04028b813f7cb6c58
ahmedazhar05 <ahmedazhar05@yahoo.com>
2020-01-08 01:26:24 +0530

    Updated/Added Features

--- sketch.js
+++ sketch.js
@@ -1,34 +1,24 @@
-let gap;
-let drawn;
 let ann;
-let temp;
 let pts;
 let shapes;
 let tools;
 let options;
 let prop;
 let sel;
+let temp;
+let drawn;
 
 function setup() {
   sel = null;
   ann = null;
   pts = 0;
+  shapes = {};
   drawn = [];
-  shapes = {
-    "Pencil":[],
-    "Ellipse":[],
-    "Circle":[],
-    "Line":[],
-    "Point":[],
-    "Quad":[],
-    "Rect":[],
-    "Square":[],
-    "Triangle":[]
+  options = {
+    "Show Grid" : {enabled : false, func : 'showGrid'},
+    "Show Ruler" : {enabled : false, func : 'showRuler'},
   };
-  options = {};
   tools={
-    "Pencil":[1],
-    //"Arc":4,
     "Ellipse":['vertical extent','horizontal extent','center'],
     "Circle":['radius','center'],
     "Line":['point','point'],
@@ -40,12 +30,12 @@ function setup() {
   };
   prop = {
     width : 600 - 2,
-    toolBarH : 30,
+    toolBarH : 40,
     toolBarY : 0,
     statusBarH : 30,
-    canvasH : 600,
     optionsBarH : 40,
   };
+  prop.canvasH = prop.width + 2;
   prop.optionsBarY = prop.toolBarY + prop.toolBarH;
   prop.canvasY = prop.optionsBarH + prop.optionsBarY;
   prop.statusBarY = prop.canvasY + prop.canvasH;
@@ -53,41 +43,102 @@ function setup() {
   createCanvas(prop.width + 2, prop.toolBarH + prop.statusBarH + prop.canvasH + prop.optionsBarH);
   textAlign(CENTER, CENTER);
   textSize(15);
-  //strokeWeight(1);
-  //stroke(0);
   noStroke();
+
+  prop.toolGap = width / Object.keys(tools).length;
+  prop.optionsGap = width / Object.keys(options).length;
 }
 
 function draw() {
   background(255);
-  showToolBar();
-  //rect(1, prop.statusBarY + 1, width - 2, prop.statusBarH - 2);
+
   strokeWeight(1);
   stroke(0);
+
   noFill();
-  rect(1, prop.canvasY, prop.width, prop.canvasH);//canvas
-  for(let i=0;i<drawn.length;i++){
-    ellipse(drawn[i].x, drawn[i].y, 10);
-    point(drawn[i]);
-  }
-  
-  if(temp && sel)
-    ellipse(temp.x, temp.y, 10);
-  for(let s in shapes){
-    for(let ob in s){
-      //window['circle'](300,400,50);
-    }
+  rect(1, prop.canvasY, prop.width +1, prop.canvasH);//canvas
+
+  for(let opt in options){
+    if(options[opt].enabled)
+      window[options[opt].func].apply(this);
   }
 
+  for(let i=0;i<drawn.length;i+=2){
+    ellipse(drawn[i], drawn[i+1], 10);
+    point(drawn[i], drawn[i+1]);
+  }
+
+  if(sel)
+    ellipse(mouseX, mouseY, 10);
+
+  /*for(let s in shapes){
+    for(let ob of shapes[s]){
+      push();
+      rectMode(CORNERS);
+      ellipseMode(CORNERS);
+      window[s.toLowerCase()].apply(this, ob);
+      pop();
+    }
+  }*/
+
   showStatusBar();
+  
+  showToolBar();
 
   showOptionsBar();
 
   showAnnotation();
 }
 
+function showGrid(){
+  push();
+  noFill();
+  strokeWeight(1);
+  for(let i = 10, inc = i; i < prop.canvasH; i += inc){
+    stroke(0,150);
+    if(i % 50 === 0)
+      stroke(0,240);
+    line(1, i + prop.canvasY, prop.width, i + prop.canvasY);
+    line(i, prop.canvasY, i, prop.canvasH + prop.canvasY);
+  }
+  pop();
+}
+
+function showRuler(){
+  push();
+  let tSize = 10;
+  textSize(tSize);
+  textAlign(CENTER, CENTER);
+  let bound = 20;
+  let thick = bound - 2;
+  let bX = 0;
+  let bY = prop.canvasY;
+  if(mouseX <= bound && mouseY >= prop.canvasY && mouseY <= prop.canvasH + prop.canvasY)
+    bX = prop.width - thick;
+  if(mouseY >= prop.canvasY && mouseY <= prop.canvasY + bound)
+    bY = prop.canvasY + prop.canvasH - thick;
+  fill(220)
+  rect(bX + 1, prop.canvasY+1, thick, prop.canvasH - 1);
+  rect(1, bY + 1, prop.width, thick);
+  fill(0);
+  noStroke();
+  let off = 2;
+  for(let i=50;i<prop.width;i+=50){
+    text(int(i/10), i-off, bY+thick/2);
+    text(int(i/10), bX+thick/2, i-off+prop.canvasY+1);
+  }
+  stroke(0);
+  fill(255);
+  rect(bX+1, bY+1, thick, thick);
+  if(mouseX >= 0 && mouseX <= prop.width+2 && mouseY >= prop.canvasY && mouseY <= prop.canvasY+prop.canvasH){
+    stroke(255,0,0);
+    line(mouseX, bY, mouseX, bY + bound);
+    line(bX, mouseY, bX + bound, mouseY);
+  }
+  pop();
+}
+
 function showToolBar(){
-  gap = width/Object.keys(tools).length;
   push();
   strokeWeight(1);
   stroke(0);
@@ -95,9 +146,9 @@ function showToolBar(){
     fill(255);
     if(sel==Object.keys(tools)[i])
       fill(220);
-    rect(i * gap + 1, prop.toolBarY, gap - 1, prop.toolBarH);
+    rect(i * prop.toolGap + 1, prop.toolBarY, prop.toolGap - 1, prop.toolBarH);
     fill(0);
-    text(Object.keys(tools)[i], (i+0.5)*gap, prop.toolBarH/2);
+    text(Object.keys(tools)[i], (i+0.5) * prop.toolGap, prop.toolBarY + prop.toolBarH / 2);
   }
   pop();
 }
@@ -106,12 +157,18 @@ function showOptionsBar(){
   push();
   strokeWeight(1);
   stroke(0);
-  const gap = Object.keys(options).length;
-  rect(1, prop.optionsBarY, prop.width, prop.optionsBarH);
+  for(let i = 0; i < Object.keys(options).length; i++){
+    fill(255);
+    if(options[Object.keys(options)[i]].enabled)
+      fill(220);
+    rect(i * prop.optionsGap + 1, prop.optionsBarY, prop.optionsGap - 1, prop.optionsBarH);
+    fill(0);
+    text(Object.keys(options)[i], (i+0.5) * prop.optionsGap, prop.optionsBarY + prop.optionsBarH / 2);
+  }
   pop();
 }
 
-function showStatusBar(){//status bar
+function showStatusBar(){
   push();
   strokeWeight(1);
   stroke(0);
@@ -128,9 +185,12 @@ function showAnnotation(){
     push();
     fill(0);
     const w = ann.length * 9;
-    rect(mouseX +10, mouseY - 20, w, 20);
+    let mX = mouseX;
+    if(mX + 10 + w > width)
+      mX = mX - w - 20;
+    rect(mX +10, mouseY - 20, w, 20);
     fill(255);
-    text(ann, mouseX + 10 +w/2, mouseY - 10);
+    text(ann, mX + 10 +w/2, mouseY - 10);
     pop();
   }
 }
@@ -138,7 +198,7 @@ function showAnnotation(){
 function mousePressed(){
   if(mouseY > prop.toolBarY && mouseY < prop.toolBarY + prop.toolBarH){
     if(!sel){
-      sel=Object.keys(tools)[floor(mouseX/gap)];
+      sel=Object.keys(tools)[floor(mouseX/prop.toolGap)];
       pts=tools[sel].length;
       ann=(tools[sel])[pts-1];
     } else {
@@ -146,6 +206,9 @@ function mousePressed(){
       pts = 0;
       ann = null;
     }
+  } else if(mouseY > prop.optionsBarY && mouseY < prop.optionsBarY+prop.optionsBarH){
+    let opt = Object.keys(options)[floor(mouseX/prop.optionsGap)];
+    options[opt].enabled = !options[opt].enabled;
   } else if(mouseY > prop.canvasY && mouseY < prop.canvasY+prop.canvasH && sel){
     temp = createVector(mouseX, mouseY);
   }
@@ -153,11 +216,17 @@ function mousePressed(){
 
 function mouseReleased(){
   if(mouseX > 0 && mouseX < width && mouseY > prop.canvasY && mouseY < prop.canvasY+prop.canvasH && sel){
-    drawn.push(createVector(mouseX,mouseY));
+    paramSett(false);
     --pts;
+    modulus = 0;
     ann=(tools[sel])[pts-1];
-    if(!pts)
+    if(!pts){
+      if(typeof(shapes[sel]) == 'undefined')
+        shapes[sel] = [];
+      shapes[sel].push(drawn);
+      drawn = [];
       sel=null;
+    }
     temp=null;
   }
 }
@@ -165,14 +234,25 @@ function mouseReleased(){
 function mouseDragged(){
   if(mouseX > 0 && mouseX < width && mouseY > prop.canvasY && mouseY < prop.canvasY+prop.canvasH && sel){
     temp = createVector(mouseX, mouseY);
+    paramSett(true);
   }
 }
 
-function keyPressed(){
-  //Delete Key deletes last point drawn
-  if(keyCode == 46){
-    drawn.pop();
-    ++pts;
-    ann=(tools[sel])[pts-1];
+function paramSett(bool){
+  if(sel == 'Circle' && pts == 1){
+    modulus = dist(drawn[drawn.length-2], drawn[drawn.length-1], temp.x, temp.y) * 2;
+  }
+  if(modulus){
+    if(bool)
+      drawn.pop();
+    drawn.push(modulus);
+  }
+  else{
+    if(bool){
+      drawn.pop();
+      drawn.pop();
+    }
+    drawn.push(temp.x);
+    drawn.push(temp.y);
   }
 }
